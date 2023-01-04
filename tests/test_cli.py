cf75f9a7f24cfaecf0116df1c8cbef312d6fc4c5
Kevin Grandjean <Muscaw@users.noreply.github.com>
2023-01-04 15:38:10 +0100

    invert test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -117,9 +117,9 @@ def test_migrate_repo_no_match(
     command = ["migrate-repo", repo_name]
     result = runner.invoke(cli, command)
 
+    assert result.exit_code != 0
     assert "Aborted!" in result.stdout
     assert f"Repository {repo_name} does not exist on Github" in result.stdout
-    assert result.exit_code != 0
     mock_list_all_repositories.assert_called_once_with(mock_get_github.return_value)
     mock_get_gitea.return_value.migrate_repo.assert_not_called()
 
