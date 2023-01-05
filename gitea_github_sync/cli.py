c21796ec984e6c7919d91089c0d38526fa7a1255
Kevin Grandjean <Muscaw@users.noreply.github.com>
2023-01-05 11:38:47 +0100

    Create sync entrypoint (#6)

--- gitea_github_sync/cli.py
+++ gitea_github_sync/cli.py
@@ -3,7 +3,7 @@ from typing import List
 import click
 from rich import print
 
-from . import config, gitea, github, repository
+from . import config, gitea, github, migration, repository
 
 
 @click.group()
@@ -61,3 +61,20 @@ def migrate_repo(full_repo_name: str) -> None:
         raise click.Abort()
 
     gt.migrate_repo(repo=repo, github_token=conf.github_token)
+
+
+@cli.command()
+def sync() -> None:
+    conf = config.load_config()
+    gt = gitea.get_gitea()
+    gh = github.get_github()
+    github_repos = github.list_all_repositories(gh)
+    gitea_repos = gt.get_repos()
+    repos_to_sync = migration.list_missing_github_repos(
+        gh_repos=github_repos, gitea_repos=gitea_repos
+    )
+    print(f"Starting migration for {len(repos_to_sync)} repos")
+    for repo in repos_to_sync:
+        print(f"Migrating [b]{repo.full_repo_name}[/]")
+        gt.migrate_repo(repo=repo, github_token=conf.github_token)
+    print(f"Migrated {len(repos_to_sync)} repos successfully")
